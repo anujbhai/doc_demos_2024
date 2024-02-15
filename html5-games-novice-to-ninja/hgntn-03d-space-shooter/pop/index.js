3751d86414e14c542069f41a2c0efa018c998476
Anuj Upadhyay <openmindedlearning@gmail.com>
2024-02-15 09:25:45 +0530

    dd2024-html-games - game lib, anim - spaceshooter, new refactored template.

---
+++ html5-games-novice-to-ninja/hgntn-03d-space-shooter/pop/index.js
@@ -0,0 +1,18 @@
+import CanvasRenderer from "./renderer/CanvasRenderer";
+import Container from "./Container"
+import KeyControls from "./controls/KeyControls";
+import MouseControls from "./controls/MouseControls";
+import Sprite from "./Sprite";
+import Text from "./Text";
+import Texture from "./Texture";
+
+export default {
+  CanvasRenderer,
+  Container,
+  KeyControls,
+  MouseControls,
+  Sprite,
+  Text,
+  Texture,
+}
+
---
+++ html5-games-novice-to-ninja/hgntn-04a-new-template/pop/Sprite.js
@@ -0,0 +1,7 @@
+export default class Sprite {
+  constructor(texture) {
+    this.texture = texture
+    this.pos = { x: 0, y: 0 }
+  }
+}
+
