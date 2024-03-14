19159b54180c440603445e7a4a0787b6ad54779e
Anuj Upadhyay <openmindedlearning@gmail.com>
2024-03-14 23:22:36 +0530

    dd2024-ts-fullstack - parcel config.

---
+++ ts-fullstack/ts-fullstack-01-setup/dist/index.377278e2.js
@@ -0,0 +1,3 @@
+console.log("yahi hain..");
+
+//# sourceMappingURL=index.377278e2.js.map
--- ts-fullstack/ts-fullstack-01-setup/index.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var sum = function (a, b) {
-        return a + b;
-    };
-    console.log('Sum: ', sum(1, 9));
-})();
