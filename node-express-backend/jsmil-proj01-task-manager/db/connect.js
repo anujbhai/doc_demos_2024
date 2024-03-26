d9434a33ccdca70985759963d4e08dfe3efd9950
Anuj Upadhyay <openmindedlearning@gmail.com>
2024-03-26 23:11:02 +0530

    dd-2024-node-express-backend - api controllers and mongo setup.

---
+++ node-express-backend/jsmil-proj01-task-manager/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString = 'mongodb+srv://openmindedlearning:adminadmin@jsdemoapps.nmvq9op.mongodb.net/?retryWrites=true&w=majority&appName=JSDemoApps'
+
--- node-express-backend/jsmil-proj01-task-manager/routes/tasks.js
+++ node-express-backend/jsmil-proj01-task-manager/routes/tasks.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const tasks = express.Router()
 
-tasks.route('/').get((req, res) => {
-  res.send('all items')
-})
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/tasks')
+
+tasks.route('/')
+  .get(getAllTasks)
+  .post(createTask)
+
+tasks.route('/:id')
+  .get(getTask)
+  .patch(updateTask)
+  .delete(deleteTask)
 
 module.exports = tasks
 
