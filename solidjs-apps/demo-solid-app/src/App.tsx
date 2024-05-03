cf7cb4854309580d223060c4469e965ebef8e00c
Anuj.Upadhyay <anuj.upadhyay@DTPN-MAC-AU-01.local>
2024-05-03 11:08:17 +0530

    dd2024-2024 - solid js demo.

---
+++ solidjs-apps/demo-solid-app/src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      Solid hello world!
+    </div>
+  );
+};
+
+export default App;
