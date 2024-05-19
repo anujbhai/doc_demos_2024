a60e91ae3517d8b13ece40f538b0123b5b0670bf
Anuj.Upadhyay <anuj.upadhyay@DTPN-MAC-AU-01.local>
2024-05-19 15:00:50 +0530

    dd2024-2024 - solid js rest app - object types.

---
+++ ts-fullstack/ts-fullstack-01-setup/src/types/l05_object_type.ts
@@ -0,0 +1,24 @@
+let person = {
+  name: "Mark",
+  age: 24,
+}
+
+let car: {
+  color: string
+  manufacturer: string
+} = {
+  color: "blue",
+  manufacturer: "Mitsubishi",
+}
+
+let article: {
+  title: string
+  content: string
+  author: string
+  img?: string
+} = {
+    author: "Lewis Caroll",
+    content: "yadayadayada",
+    title: "The yada of yadayada",
+  }
+
