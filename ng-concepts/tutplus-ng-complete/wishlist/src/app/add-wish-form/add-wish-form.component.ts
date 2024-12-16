3bf4c58b36a9278d5a56ededeee3c5e0a1eb2205
kkesseler <kkesseler@ifeelsmart.com>
2024-12-16 18:49:32 +0530

    ng-quest - tutsplus - getters, input, output, onInit..

---
+++ ng-concepts/tutplus-ng-complete/wishlist/src/app/add-wish-form/add-wish-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { WishList } from '../..//shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'add-wish-form',
+  imports: [FormsModule],
+  templateUrl: './add-wish-form.component.html',
+  styleUrl: './add-wish-form.component.css'
+})
+export class AddWishFormComponent {
+  @Output() addWish = new EventEmitter<WishList>();
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  newWishText = '';
+
+  addNewWish() {
+    this.addWish.emit(new WishList(this.newWishText))
+    this.newWishText = '';
+  }
+}
