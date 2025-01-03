adeaaaf6df0a81e32fdf0514efb19d20221e954a
kkesseler <kkesseler@ifeelsmart.com>
2025-01-03 17:28:14 +0530

    ng-quest - packt-ng-patts - starter app and shared imports.

---
+++ ng-concepts/packt-ng-design-patts-best-practices/angular-start/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { sharedImports } from '../../shared/shared-imports';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  standalone: true,
+  imports: [
+    sharedImports,
+  ]
+})
+export class HomeComponent {
+}
