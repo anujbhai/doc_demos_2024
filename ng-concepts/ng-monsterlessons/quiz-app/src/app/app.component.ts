f381825576d0f4a95210783736abec07539d43eb
kkesseler <kkesseler@ifeelsmart.com>
2025-02-03 12:02:10 +0530

    ng-quest - monsterlessons - creating components.

---
+++ ng-concepts/ng-monsterlessons/quiz-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    QuizComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {}
---
+++ ng-concepts/ng-monsterlessons/quiz-app/src/app/quiz/components/answers/answers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'quiz-answers',
+  templateUrl: './answers.component.html',
+  styleUrl: './answers.component.css',
+})
+export class AnswersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+
+
